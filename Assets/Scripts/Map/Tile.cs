ba1a78ed1e30b076feb397dbf31de4d0b09a7580
shin, seong june <69497751+shinseongjune@users.noreply.github.com>
2022-06-07 18:20:58 +0900

    tile

--- Assets/Scripts/Map/Tile.cs
+++ Assets/Scripts/Map/Tile.cs
@@ -1,18 +1,44 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class Tile : MonoBehaviour
+public struct Tile
 {
-    // Start is called before the first frame update
-    void Start()
+    public int x;
+    public int y;
+
+    public Vector3 position
     {
-        
+        get
+        {
+            return new Vector3(x, 0, y);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public TileLine topLine;
+    public TileLine bottomLine;
+    public TileLine leftLine;
+    public TileLine rightLine;
+
+    public Tile(int x, int y, TileLine topLine, TileLine bottomLine, TileLine leftLine, TileLine rightLine)
     {
-        
+        this.x = x;
+        this.y = y;
+
+        this.topLine = topLine;
+        this.bottomLine = bottomLine;
+        this.leftLine = leftLine;
+        this.rightLine = rightLine;
+    }
+}
+
+public class TileLine
+{
+    public Vector3 position;
+
+    public bool isHorizontal;
+
+    public TileLine(Vector3 position, bool isHorizontal)
+    {
+        this.position = position;
+        this.isHorizontal = isHorizontal;
     }
 }
