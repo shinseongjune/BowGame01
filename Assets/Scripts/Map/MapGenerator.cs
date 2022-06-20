c5280f068f41e3e3c16ec214bd013aa939318c83
shin, seong june <69497751+shinseongjune@users.noreply.github.com>
2022-06-20 18:33:00 +0900

    stairs

--- Assets/Scripts/Map/MapGenerator.cs
+++ Assets/Scripts/Map/MapGenerator.cs
@@ -5,6 +5,26 @@ using UnityEngine.AI;
 
 public class MapGenerator : MonoBehaviour
 {
+    class Room
+    {
+        public List<MapPiece> mapPieces = new List<MapPiece>();
+        public List<MapPiece> edges = new List<MapPiece>();
+
+        public List<Room> connectedRooms = new List<Room>();
+
+        public bool IsConnected(Room room)
+        {
+            return connectedRooms.Contains(room);
+        }
+    }
+
+    class StairPoint
+    {
+        public int x;
+        public int z;
+        public MapPiece.Direction direction = MapPiece.Direction.None;
+    }
+
     public GameObject groundPrefab;
     public GameObject stairsPrefab;
     public GameObject rockPrefab;
@@ -20,8 +40,8 @@ public class MapGenerator : MonoBehaviour
 
     public static float TILE_XZ;
     public static float TILE_HEIGHT;
-    public const int GRID_X = 256;
-    public const int GRID_Y = 256;
+    public const int GRID_X = 30;
+    public const int GRID_Y = 30;
 
     private void Start()
     {
@@ -117,6 +137,10 @@ public class MapGenerator : MonoBehaviour
 
         SmoothingMap();
 
+        //List<Room> rooms = GetRooms();
+
+        //List<StairPoint> points = MakeStairsPositions(rooms);
+
         for (int x = 0; x < GRID_X; x++)
         {
             for (int z = 0; z < GRID_Y; z++)
@@ -136,19 +160,331 @@ public class MapGenerator : MonoBehaviour
             }
         }
 
+        //foreach(StairPoint point in points)
+        //{
+        //    float angle = 0f;
+        //
+        //    switch (point.direction)
+        //    {
+        //        case MapPiece.Direction.North:
+        //            angle = 0f;
+        //            break;
+        //        case MapPiece.Direction.South:
+        //            angle = 180f;
+        //            break;
+        //        case MapPiece.Direction.West:
+        //            angle = 270f;
+        //            break;
+        //        case MapPiece.Direction.East:
+        //            angle = 90f;
+        //            break;
+        //    }
+        //
+        //    GameObject stairs = Instantiate(stairsPrefab, new Vector3(point.x * TILE_XZ + TILE_XZ / 2, TILE_HEIGHT, point.z * TILE_XZ + TILE_XZ / 2), Quaternion.AngleAxis(angle, Vector3.up));
+        //    stairs.transform.SetParent(transform, false);
+        //}
+
         NavMeshSurface navMeshSurface = transform.GetChild(0).GetComponent<NavMeshSurface>();
         if (navMeshSurface != null)
         {
-            navMeshSurface.BuildNavMesh();
+            //navMeshSurface.BuildNavMesh();
         }
         //foreach (Transform mapPiece in transform)
         //{
-            //NavMeshSurface navMeshSurface = mapPiece.GetComponent<NavMeshSurface>();
-            //if (navMeshSurface != null)
-            //{
-            //    navMeshSurface.BuildNavMesh();
-            //}
+        //NavMeshSurface navMeshSurface = mapPiece.GetComponent<NavMeshSurface>();
+        //if (navMeshSurface != null)
+        //{
+        //    navMeshSurface.BuildNavMesh();
         //}
+        //}
+    }
+
+    private List<StairPoint> MakeStairsPositions(List<Room> rooms)
+    {
+        foreach (Room room in rooms)
+        {
+            if (room.mapPieces[0].y == 0)
+            {
+                continue;
+            }
+            foreach(MapPiece piece in room.mapPieces)
+            {
+                int y = piece.y;
+
+                if (piece.x > 0 && heightMap[piece.x - 1, piece.z].y != y) //왼쪽
+                {
+                    piece.direction |= MapPiece.Direction.West;
+                }
+
+                if (piece.x < GRID_X - 1 && heightMap[piece.x + 1, piece.z].y != y) //오른쪽
+                {
+                    piece.direction |= MapPiece.Direction.East;
+                }
+
+                if (piece.z < GRID_Y - 1 && heightMap[piece.x, piece.z + 1].y != y) //위
+                {
+                    piece.direction |= MapPiece.Direction.North;
+                }
+
+                if (piece.z > 0 && heightMap[piece.x, piece.z - 1].y != y) //아래
+                {
+                    piece.direction |= MapPiece.Direction.South;
+                }
+
+                if (piece.direction != MapPiece.Direction.None)
+                {
+                    room.edges.Add(piece);
+                }
+            }
+        }
+
+        List<StairPoint> points = new();
+
+        foreach (Room room in rooms)
+        {
+            if (room.mapPieces[0].y == 0)
+            {
+                continue;
+            }
+            bool isMakingPoint = true;
+            while(isMakingPoint)
+            {
+                int index = Random.Range(0, room.edges.Count);
+                int rand;
+
+                MapPiece piece = room.edges[index];
+
+                switch (piece.direction)
+                {
+                    case MapPiece.Direction.NorthWest:
+                        rand = Random.Range(0, 2);
+                        if (rand == 0)
+                        {
+                            if (piece.z + 2 < GRID_Y && heightMap[piece.x, piece.z +2].y != piece.y)
+                            {
+                                StairPoint point = new();
+                                point.x = piece.x;
+                                point.z = piece.z + 1;
+                                point.direction = MapPiece.Direction.North;
+                                points.Add(point);
+                                isMakingPoint = false;
+                            }
+                        }
+                        else
+                        {
+                            if (piece.x - 2 >= 0 && heightMap[piece.x - 1, piece.z].y != piece.y)
+                            {
+                                StairPoint point = new();
+                                point.x = piece.x - 1;
+                                point.z = piece.z;
+                                point.direction = MapPiece.Direction.West;
+                                points.Add(point);
+                                isMakingPoint = false;
+                            }
+                        }
+                        break;
+                    case MapPiece.Direction.SouthWest:
+                        rand = Random.Range(0, 2);
+                        if (rand == 0)
+                        {
+                            if (piece.z - 2 >= 0 && heightMap[piece.x, piece.z - 2].y != piece.y)
+                            {
+                                StairPoint point = new();
+                                point.x = piece.x;
+                                point.z = piece.z - 1;
+                                point.direction = MapPiece.Direction.South;
+                                points.Add(point);
+                                isMakingPoint = false;
+                            }
+                        }
+                        else
+                        {
+                            if (piece.x - 2 >= 0 && heightMap[piece.x - 1, piece.z].y != piece.y)
+                            {
+                                StairPoint point = new();
+                                point.x = piece.x - 1;
+                                point.z = piece.z;
+                                point.direction = MapPiece.Direction.West;
+                                points.Add(point);
+                                isMakingPoint = false;
+                            }
+                        }
+                        break;
+                    case MapPiece.Direction.NorthEast:
+                        rand = Random.Range(0, 2);
+                        if (rand == 0)
+                        {
+                            if (piece.z + 2 < GRID_Y && heightMap[piece.x, piece.z + 2].y != piece.y)
+                            {
+                                StairPoint point = new();
+                                point.x = piece.x;
+                                point.z = piece.z + 1;
+                                point.direction = MapPiece.Direction.North;
+                                points.Add(point);
+                                isMakingPoint = false;
+                            }
+                        }
+                        else
+                        {
+                            if (piece.x + 2 < GRID_X && heightMap[piece.x + 2, piece.z].y != piece.y)
+                            {
+                                StairPoint point = new();
+                                point.x = piece.x + 1;
+                                point.z = piece.z;
+                                point.direction = MapPiece.Direction.East;
+                                points.Add(point);
+                                isMakingPoint = false;
+                            }
+                        }
+                        break;
+                    case MapPiece.Direction.SouthEast:
+                        rand = Random.Range(0, 2);
+                        if (rand == 0)
+                        {
+                            if (piece.z - 2 >= 0 && heightMap[piece.x, piece.z - 2].y != piece.y)
+                            {
+                                StairPoint point = new();
+                                point.x = piece.x;
+                                point.z = piece.z - 1;
+                                point.direction = MapPiece.Direction.South;
+                                points.Add(point);
+                                isMakingPoint = false;
+                            }
+                        }
+                        else
+                        {
+                            if (piece.x + 2 < GRID_X && heightMap[piece.x + 2, piece.z].y != piece.y)
+                            {
+                                StairPoint point = new();
+                                point.x = piece.x + 1;
+                                point.z = piece.z;
+                                point.direction = MapPiece.Direction.East;
+                                points.Add(point);
+                                isMakingPoint = false;
+                            }
+                        }
+                        break;
+                    case MapPiece.Direction.North:
+                        if (piece.z + 2 < GRID_Y && heightMap[piece.x, piece.z + 2].y != piece.y)
+                        {
+                            StairPoint point = new();
+                            point.x = piece.x;
+                            point.z = piece.z + 1;
+                            point.direction = MapPiece.Direction.North;
+                            points.Add(point);
+                            isMakingPoint = false;
+                        }
+                        break;
+                    case MapPiece.Direction.South:
+                        if (piece.z - 2 >= 0 && heightMap[piece.x, piece.z - 2].y != piece.y)
+                        {
+                            StairPoint point = new();
+                            point.x = piece.x;
+                            point.z = piece.z - 1;
+                            point.direction = MapPiece.Direction.South;
+                            points.Add(point);
+                            isMakingPoint = false;
+                        }
+                        break;
+                    case MapPiece.Direction.West:
+                        if (piece.x - 2 >= 0 && heightMap[piece.x - 1, piece.z].y != piece.y)
+                        {
+                            StairPoint point = new();
+                            point.x = piece.x - 1;
+                            point.z = piece.z;
+                            point.direction = MapPiece.Direction.West;
+                            points.Add(point);
+                            isMakingPoint = false;
+                        }
+                        break;
+                    case MapPiece.Direction.East:
+                        if (piece.x + 2 < GRID_X && heightMap[piece.x + 2, piece.z].y != piece.y)
+                        {
+                            StairPoint point = new();
+                            point.x = piece.x + 1;
+                            point.z = piece.z;
+                            point.direction = MapPiece.Direction.East;
+                            points.Add(point);
+                            isMakingPoint = false;
+                        }
+                        break;
+                }
+            }
+        }
+
+        return points;
+    }
+
+    private List<Room> GetRooms()
+    {
+        List<Room> rooms = new();
+
+        int[,] flags = new int[GRID_X, GRID_Y];
+
+        for (int x = 0; x < GRID_X; x++)
+        {
+            for (int z = 0; z < GRID_Y; z++)
+            {
+                if (flags[x,z] == 0)
+                {
+                    flags[x, z] = 1;
+
+                    Room room = new();
+
+                    Queue<MapPiece> checkNow = new();
+                    Queue<MapPiece> checkNext = new();
+
+                    checkNow.Enqueue(heightMap[x, z]);
+
+                    while(checkNow.Count > 0)
+                    {
+                        MapPiece piece = checkNow.Dequeue();
+                        int y = piece.y;
+                        room.mapPieces.Add(piece);
+
+                        for (int offsetX = piece.x - 1; offsetX <= piece.x + 1; offsetX++)
+                        {
+                            for (int offsetZ = piece.z - 1; offsetZ <= piece.z + 1; offsetZ++)
+                            {
+                                if (offsetX < 0 || offsetZ < 0 || offsetX >= GRID_X || offsetZ >= GRID_Y)
+                                {
+                                    continue;
+                                }
+                                if (offsetX == piece.x && offsetZ == piece.z)
+                                {
+                                    continue;
+                                }
+                                if (flags[offsetX, offsetZ] == 1)
+                                {
+                                    continue;
+                                }
+
+                                if (offsetX == piece.x || offsetZ == piece.z)
+                                {
+                                    flags[offsetX, offsetZ] = 1;
+                                    
+                                    if (heightMap[offsetX, offsetZ].y == y)
+                                    {
+                                        checkNext.Enqueue(heightMap[offsetX, offsetZ]);
+                                    }
+                                }
+                            }
+                        }
+
+                        if (checkNow.Count == 0)
+                        {
+                            var temp = checkNow;
+                            checkNow = checkNext;
+                            checkNext = temp;
+                        }
+                    }
+
+                    rooms.Add(room);
+                }
+            }
+        }
+
+        return rooms;
     }
 
     public void SmoothingMap()
